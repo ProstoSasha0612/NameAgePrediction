0a41600e9fe21e77f4e0400cb435809b3e08ddd7
ProstoSasha0612 <alexandr.popko.2013@mail.ru>
2022-12-28 17:13:19 +0300

    init project

---
+++ app/src/main/java/com/projectapp/nameageprediction/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.projectapp.nameageprediction
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.projectapp.nameageprediction.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        binding.navView.setupWithNavController(navController)
+    }
+}
