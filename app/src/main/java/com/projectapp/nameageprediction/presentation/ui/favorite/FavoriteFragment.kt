ffd429b22078555c8d866bc41902b941e58c0375
ProstoSasha0612 <alexandr.popko.2013@mail.ru>
2023-01-07 19:26:54 +0300

    add: delete from favorite feature (without dialogue)

--- app/src/main/java/com/projectapp/nameageprediction/presentation/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/projectapp/nameageprediction/presentation/ui/favorite/FavoriteFragment.kt
@@ -2,25 +2,17 @@ package com.projectapp.nameageprediction.presentation.ui.favorite
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.projectapp.nameageprediction.databinding.FragmentFavoriteBinding
-import com.projectapp.nameageprediction.domain.repository.Repository
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class FavoriteFragment : Fragment() {
@@ -44,6 +36,7 @@ class FavoriteFragment : Fragment() {
         setUpRecyclerView()
         observeDeleteBtnVisibility()
         setOnDeleteBtnClickListener()
+        viewModel.observeFavoritesList()
     }
 
     private fun setUpRecyclerView() {
@@ -75,6 +68,8 @@ class FavoriteFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
+        binding.recyclerView.adapter = null
         _binding = null
+
     }
 }
--- app/src/main/java/com/projectapp/nameageprediction/presentation/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/projectapp/nameageprediction/presentation/ui/favorite/FavoriteViewModel.kt
@@ -1,15 +1,14 @@
 package com.projectapp.nameageprediction.presentation.ui.favorite
 
 import android.annotation.SuppressLint
-import androidx.lifecycle.*
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.projectapp.nameageprediction.domain.models.NameAgePrediction
 import com.projectapp.nameageprediction.domain.usecases.DeleteFromFavoriteUseCase
 import com.projectapp.nameageprediction.domain.usecases.GetFavoritesPredictionsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -21,34 +20,31 @@ class FavoriteViewModel @Inject constructor(
     private val deleteFromFavoriteUseCase: DeleteFromFavoriteUseCase,
 ) : ViewModel() {
 
-    //    private val _showDeleteButtonFlow: MutableStateFlow<Boolean> = predictionsAdapter.isShowCheckboxesState
     val showDeleteButtonState: StateFlow<Boolean> = _predictionsAdapter.isShowCheckboxesState
     val predictionsAdapter: ListAdapter<NameAgePrediction, PredictionsRecyclerViewAdapter.PredictionsViewHolder>
         get() = _predictionsAdapter
 
-    init {
-        observeFavoritesList()
-    }
-
     @SuppressLint("NotifyDataSetChanged")
     fun deleteSelectedItems() {
-
         viewModelScope.launch {
-//            deleteFromFavoriteUseCase(_predictionsAdapter.currentList)
+            _predictionsAdapter.currentList.forEach { prediction ->
+                if (prediction.isChecked) {
+                    deleteFromFavoriteUseCase(prediction)
+                }
+            }
+            getFavoritesPredictionsUseCase().collect { list ->
+                Log.d("MYTAG","DATA COLLECTED")
+                predictionsAdapter.submitList(list)
+            }
         }
-
-        _predictionsAdapter.currentList.forEach { prediction ->
-        }
-
-
         _predictionsAdapter.isShowCheckboxesState.value = false
         _predictionsAdapter.notifyDataSetChanged()
     }
 
-
     fun observeFavoritesList() {
         viewModelScope.launch {
             getFavoritesPredictionsUseCase().collect { list ->
+                Log.d("MYTAG","DATA COLLECTED")
                 predictionsAdapter.submitList(list)
             }
         }
